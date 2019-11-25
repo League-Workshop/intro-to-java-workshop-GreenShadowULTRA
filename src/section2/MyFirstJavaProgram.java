1c5e01acc46105ed1f5d0e01ca9a2f26dfbadfdc
League Student <league@iMac-12.attlocal.net>
2019-11-25 15:28:04 -0800

    ROOOOOOBOTye
    Weird Computer thingy that is cool yea

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+	Robot ree=new Robot();
+		ree.hide();
+		ree.penDown();
+		ree.setPenWidth(500);
+		ree.setPenColor(10,255,100);
+		ree.setSpeed(200);
 		
-		
+		for(int i=0;i<4;i++) {
+			
+			ree.move(200);
+			ree.turn(90);
+		}
 	}
 }
